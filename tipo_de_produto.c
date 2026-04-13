51850c2fad526c34027815981a3d3c6776020fa0
Lucas Proença <lucas.proencab98@gmail.com>
2026-04-13 16:29:55 -0300

    Exercício livro Forbellone
    algoritmo que informa o tipo de produto selecionado

---
+++ tipo_de_produto.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int codigo;
+    printf("Digite o codigo do produto:\n");
+    scanf("%d", &codigo);
+    if (codigo == 1){
+        printf("Alimento nao perecivel\n");
+    }
+    else if(codigo == 2 || codigo == 3 || codigo == 4){
+        printf("Alimento perecivel\n");
+    }
+    else if(codigo == 5 || codigo == 6){
+        printf("Vestuario\n");
+    }
+    else if(codigo == 7){
+        printf("Higiene pessoal\n");
+    }
+     else if(codigo >= 8 && codigo <= 15){
+        printf("Limpeza e untesilios domesticos\n");
+    }
+    else{
+        printf("Invalido\n");
+    }
+}
